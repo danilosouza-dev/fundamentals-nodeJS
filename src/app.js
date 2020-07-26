501d1d9fb625b32f159dcbcfcf2ecc51474951c6
Danilo da Silva <gtd.danilosouza@gmail.com>
2020-07-26 01:50:25 -0300

    Test rename repository

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = []
 
 function validateId(req, res, next) {
   const { id } = req.params;
